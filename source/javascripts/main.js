289d6095ab6e02307a2e048455dbb7fef5664252
CH <calbach88@gmail.com>
2012-12-09 22:08:33 -0800

    Change scores to be dynamically pulled from the spreadsheet.

---
+++ source/javascripts/main.js
@@ -0,0 +1,13 @@
+var POINTS_FEED_URL = 'https://spreadsheets.google.com/' +
+    'feeds/cells/0Aong3uq5I0qWdHlTZExDblc2cEZnZWl4OTdoMzFTUGc/' +
+    'od6/public/basic?range=B15%3AC15&alt=rss';
+
+$(function() {
+  $.getFeed({
+    url: POINTS_FEED_URL,
+    success: function(feed) {
+      $('.ch .score').text(feed.items[0].description);
+      $('.alex .score').text(feed.items[1].description);
+    }
+  });
+});
