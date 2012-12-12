7d4c00911a9809de59df17489bc3671a16132f1a
Alex Baldwin <alexbaldwin@gmail.com>
2012-12-12 09:36:09 -0800

    testing

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,12 +2,29 @@ var POINTS_FEED_URL = 'https://spreadsheets.google.com/' +
     'feeds/cells/0Aong3uq5I0qWdHlTZExDblc2cEZnZWl4OTdoMzFTUGc/' +
     'od6/public/basic?range=B15%3AC15&alt=rss';
 
-$(function() {
-  $.getFeed({
-    url: POINTS_FEED_URL,
-    success: function(feed) {
-      $('.player.one .score').text(feed.items[0].description);
-      $('.player.two .score').text(feed.items[1].description);
-    }
+
+
+
+setInterval(function(){
+
+  
+  
+  
+  $(function() {
+    $.getFeed({
+      url: POINTS_FEED_URL,
+      success: function(feed) {
+        $('.player.one .score').text(feed.items[0].description);
+        $('.player.two .score').text(feed.items[1].description);
+      }
+    });
   });
-});
+  
+  
+  
+  
+  
+  
+  
+  
+}, 300);
